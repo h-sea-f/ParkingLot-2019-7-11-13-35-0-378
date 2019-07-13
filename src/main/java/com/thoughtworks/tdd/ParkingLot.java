69b0cbba1ee85c77e21fe84be1ebe2752bf98964
h-sea-f <h_sea_f@163.com>
2019-07-13 17:09:52 +0800

    finish should_return_a_ticket_when_park_a_car implements

--- src/main/java/com/thoughtworks/tdd/ParkingLot.java
+++ src/main/java/com/thoughtworks/tdd/ParkingLot.java
@@ -11,7 +11,9 @@ public class ParkingLot {
     private Map<ParkingTicket, Car> cars = new HashMap<ParkingTicket, Car>();
 
     public ParkingTicket parkCar(Car car){
-
+        ParkingTicket ticket=new ParkingTicket(car);
+        this.cars.put(ticket,car);
+        return ticket;
     }
     public ParkingLot(int capacity) {
         this.capacity = capacity;

f9ad166aaf65aa987d3ce1d4106d39d10adcc4de
h-sea-f <h_sea_f@163.com>
2019-07-13 17:31:21 +0800

    finish should_return_a_car_when_takeCar test

--- src/main/java/com/thoughtworks/tdd/Car.java
+++ src/main/java/com/thoughtworks/tdd/Car.java
@@ -1,4 +1,14 @@
 package com.thoughtworks.tdd;
-
 public class Car {
+    private int carId;
+    public Car(){
+
+    }
+    public Car(int carId){
+        this.carId=carId;
+    }
+
+    public int getCarId() {
+        return carId;
+    }
 }
--- src/test/java/com/thoughtworks/tdd/ParkingLotTest.java
+++ src/test/java/com/thoughtworks/tdd/ParkingLotTest.java
@@ -1,10 +1,12 @@
 package com.thoughtworks.tdd;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class ParkingLotTest {
     @Test
+    @DisplayName("shoule return a ticket when park a car")
     public void should_return_a_ticket_when_park_a_car() {
         //given
         ParkingLot parkingLot = new ParkingLot();
@@ -14,4 +16,18 @@ public class ParkingLotTest {
         //then
         Assertions.assertEquals(ticket.getCar(), car);
     }
+
+    @Test
+    @DisplayName("shoule return a ticket when park a car")
+    public void should_return_a_car_when_takeCar() {
+        //given
+        ParkingLot parkingLot = new ParkingLot();
+        Car car = new Car(123);
+        ParkingTicket ticket = parkingLot.parkCar(car);
+        //when
+        //then
+        Assertions.assertEquals(ticket.getCar().getCarId(), car.getCarId());
+    }
+
+
 }

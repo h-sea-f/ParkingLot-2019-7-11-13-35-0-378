b57250ab3aef763140a82ecc93edeacd5419f62f
h-sea-f <h_sea_f@163.com>
2019-07-14 10:11:39 +0800

    finish SuperSmartParkingBoy

--- src/main/java/com/thoughtworks/tdd/SuperSmartParkingBoy.java
+++ src/main/java/com/thoughtworks/tdd/SuperSmartParkingBoy.java
@@ -1,4 +1,16 @@
 package com.thoughtworks.tdd;
 
+import java.util.List;
+
 public class SuperSmartParkingBoy {
+    public ParkingTicket paringBoyParkCar(Car car, List<ParkingLot> parkingLotList) {
+        ParkingLot lowestUtilizationParkingLot = parkingLotList.get(0);
+        for (ParkingLot parkingLot : parkingLotList) {
+            boolean isLowestUtilizationParkingLot = (parkingLot.getCars().size() / parkingLot.getCapacity()) < (lowestUtilizationParkingLot.getCars().size() / lowestUtilizationParkingLot.getCapacity());
+            if (isLowestUtilizationParkingLot) {
+                lowestUtilizationParkingLot = parkingLot;
+            }
+        }
+        return lowestUtilizationParkingLot.parkCar(car);
+    }
 }
